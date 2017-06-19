23030d34f8e40bdbddedabdd537695a7b604fbcf
Delfin Pereiro Parejo <pereiro@qumram.ch>
2017-06-19 15:05:28 +0200

    [UPDATE]: Uses APN instead of node-pushnotifications module. It works!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 const PushNotifications = new require('node-pushnotifications');
+const apn = require('apn');
  
 var app = express()
  
@@ -18,14 +19,22 @@ const settings = {
         //     keyId: 'ABCD',
         //     teamId: 'EFGH',
         // },
-        cert: './certs/devCert.pem',
+        // cert: './certs/devCert.pem',
         // key: './certs/devKey.pem',
         passphrase :'qumram',
         production : false
     }
 };
 
-const push = new PushNotifications(settings);
+// const push = new PushNotifications(settings);
+
+    const options = {
+        cert: './certs/devCert.pem',
+        key: './certs/devKey.pem',
+        passphrase :'qumram',
+        production : false
+    };
+    const apnProvider = new apn.Provider(options);
 
 //------------------------------------------------------------------------------
 // Configure push Notification
@@ -70,23 +79,44 @@ const data = {
 };
 
 // Push destination devices
-const registrationIds = [];
-registrationIds.push('f43abe6fb70545a0d80b88e3cf1329ec7f42b35a');
-registrationIds.push('801441d435155c0c869b0c03192d6325dc05020d');
+// const registrationIds = [];
+// registrationIds.push('f43abe6fb70545a0d80b88e3cf1329ec7f42b35a');
+// registrationIds.push('801441d435155c0c869b0c03192d6325dc05020d');
+
+let deviceToken = "2eac69a08879fe893475e83860b339b4c8af1aa5fd52cf613b709d5a270d22fa";
 
 //------------------------------------------------------------------------------
 // Send push!!!
 //------------------------------------------------------------------------------
-push.send(registrationIds, data, (err, result) => {
-    if (err) {
-        console.log(err);
+// push.send(registrationIds, data, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//       const answer = result[0];
+//       if ( answer.failure >= 0 ){
+//         console.log(answer.message);        
+//       } else {
+//         console.log("Lo logre!!!!");
+//       }
+//     }
+// });
+
+var note = new apn.Notification();
+
+note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+note.badge = 1;
+note.sound = "ping.aiff";
+note.alert = "This is a new message from NodeJS";
+note.payload = {'messageFrom': 'John Appleseed'};
+note.topic = "com.qumram.whatsappRecordStatus";
+
+apnProvider.send(note, deviceToken).then( (result) => {
+  // see documentation for an explanation of result    
+    if ( result.failed.length > 0 ){
+        const failed = result.failed[0];
+        console.log(failed);
     } else {
-      const answer = result[0];
-      if ( answer.failure >= 0 ){
-        console.log(answer.message);        
-      } else {
         console.log("Lo logre!!!!");
-      }
     }
 });
 
